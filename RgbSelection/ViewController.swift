23a285e01cd00e0f40faa0c6cd34d0d76bb9627d
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2021-01-05 16:48:43 +0300

    start logic

--- RgbSelection/ViewController.swift
+++ RgbSelection/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var redSlider: UIView!
+    @IBOutlet var greenSlider: UIView!
+    @IBOutlet var blueSlider: UIView!
+    @IBOutlet var valueRed: UILabel!
+    @IBOutlet var valueGreen: UILabel!
+    @IBOutlet var valueBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.value = 2
+        valueGreen.text = "1"
+        valueBlue.text = "1"
     }
 
-
+    @IBAction func changedRedSlider() {
+    }
+    
 }
 
