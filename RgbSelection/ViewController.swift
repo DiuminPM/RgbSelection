012cd8ce7644761889375bfd65402f9a93d569f3
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2021-01-06 13:36:48 +0300

    Final logic

--- RgbSelection/ViewController.swift
+++ RgbSelection/ViewController.swift
@@ -9,22 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var mainView: UIView!
-    @IBOutlet var redSlider: UIView!
-    @IBOutlet var greenSlider: UIView!
-    @IBOutlet var blueSlider: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     @IBOutlet var valueRed: UILabel!
     @IBOutlet var valueGreen: UILabel!
     @IBOutlet var valueBlue: UILabel!
+    @IBOutlet var mainLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redSlider.value = 2
-        valueGreen.text = "1"
-        valueBlue.text = "1"
-    }
-
-    @IBAction func changedRedSlider() {
+        mainView.layer.cornerRadius = 10
+        valueRed.text = String(Int(redSlider.value))
+        valueGreen.text = String(Int(greenSlider.value))
+        valueBlue.text = "\(Int(blueSlider.value))"
     }
     
+    @IBAction func changedRGBColor() {
+        valueRed.text = String(Int(redSlider.value))
+        valueGreen.text = String(Int(greenSlider.value))
+        valueBlue.text = "\(Int(blueSlider.value))"
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1)
+    }
 }
 
